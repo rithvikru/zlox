a512f469a0c4413d577a673b597261e3b9ae3f48
Rithvik Redrouthu <rithvik.redrouthu@gmail.com>
2025-04-17 16:50:27 -0400

    minimal scanner done

---
+++ src/main.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+const Scanner = @import("scanner.zig");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    switch (args.len) {
+        2 => try runFile(allocator, args[1]),
+        else => {
+            const stderr = std.io.getStdErr().writer();
+            try stderr.print("Usage: zlox [path]\n", .{});
+            std.process.exit(64);
+        },
+    }
+}
+
+fn runFile(allocator: std.mem.Allocator, path: []const u8) !void {
+    const source = try std.fs.cwd().readFileAlloc(allocator, path, 1_000_000);
+    defer allocator.free(source);
+    try run(allocator, source);
+}
+
+fn run(allocator: std.mem.Allocator, source: []const u8) !void {
+    var scanner = Scanner.init(allocator, source);
+    const tokens = try scanner.scanTokens();
+    for (tokens.items) |token| {
+        std.debug.print("{} ({s})\n", .{ token.ttype, token.lexeme });
+    }
+}
---
+++ src/scanner.zig
@@ -0,0 +1,140 @@
+const std = @import("std");
+
+const Scanner = @This();
+
+source: []const u8,
+tokens: std.ArrayList(Token),
+start: usize,
+current: usize,
+line: usize,
+
+pub fn init(allocator: std.mem.Allocator, source: []const u8) Scanner {
+    return Scanner{
+        .source = source,
+        .tokens = std.ArrayList(Token).init(allocator),
+        .start = 0,
+        .current = 0,
+        .line = 1,
+    };
+}
+
+fn isAtEnd(self: *Scanner) bool {
+    return self.current >= self.source.len;
+}
+
+fn peek(self: *Scanner) u8 {
+    if (self.isAtEnd()) {
+        return 0;
+    }
+    return self.source[self.current];
+}
+
+fn advance(self: *Scanner) void {
+    self.current += 1;
+}
+
+pub fn makeError(self: *Scanner, message: []const u8) Token {
+    return Token{
+        .ttype = .ERROR,
+        .lexeme = message,
+        .line = self.line,
+    };
+}
+
+fn makeToken(self: *Scanner, ttype: TokenType) Token {
+    return Token{
+        .ttype = ttype,
+        .lexeme = self.source[self.start..self.current],
+        .line = self.line,
+    };
+}
+
+fn scanToken(self: *Scanner) Token {
+    self.start = self.current;
+    if (self.isAtEnd()) {
+        return self.makeToken(.EOF);
+    }
+
+    const c = self.peek();
+    self.advance();
+
+    return switch (c) {
+        '(' => self.makeToken(.LEFT_PAREN),
+        ')' => self.makeToken(.RIGHT_PAREN),
+        '{' => self.makeToken(.LEFT_BRACE),
+        '}' => self.makeToken(.RIGHT_BRACE),
+        ',' => self.makeToken(.COMMA),
+        '.' => self.makeToken(.DOT),
+        '-' => self.makeToken(.MINUS),
+        '+' => self.makeToken(.PLUS),
+        ';' => self.makeToken(.SEMICOLON),
+        '*' => self.makeToken(.STAR),
+        else => {
+            return self.makeError("Unexpected character");
+        },
+    };
+}
+
+pub fn scanTokens(self: *Scanner) !std.ArrayList(Token) {
+    while (!self.isAtEnd()) {
+        try self.tokens.append(self.scanToken());
+    }
+    return self.tokens;
+}
+
+pub const Token = struct {
+    ttype: TokenType,
+    lexeme: []const u8,
+    line: usize,
+};
+
+pub const TokenType = enum {
+    // Single-character tokens.
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    // One or two character tokens.
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    // Keywords.
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+
+    ERROR,
+    EOF,
+};
